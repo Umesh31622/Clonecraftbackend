ced963612eef65a40063dd763deb3493b09a4efa
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-10-24 18:19:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://thriving-pony-410ad8.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
