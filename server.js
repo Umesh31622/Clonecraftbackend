2b2514f234338d42b1ce55a52c164256fe13e152
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-10-25 12:59:42 +0530

    Disable CORS middleware in server.js
    Comment out CORS middleware configuration.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@
 // app.use(helmet());
 // app.use(
 //   cors({
-//     origin: process.env.FRONTEND_URL || "https://clonecrafr.vercel.app",
+//     origin: process.env.FRONTEND_URL || "",
 //     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 //     allowedHeaders: ["Content-Type", "Authorization"],
 //     credentials: true,
