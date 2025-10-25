2144cf78a4cafd872357e41d803869d9e9a4ada8
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-10-25 13:01:56 +0530

    Update CORS origin for local development

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://clonecrafr.vercel.app",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
