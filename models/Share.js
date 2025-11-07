fb40162dd2f0e30d63179eaf0ada1844a93b1b83
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-07 14:52:11 +0530

    Update Share.js

--- models/Share.js
+++ models/Share.js
@@ -1,1 +1,21 @@
+const mongoose = require("mongoose");
+
+const shareSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    isProfile: { type: Boolean, default: false },
+    category: { type: String, default: "" },
+    file: { type: String, default: "" },
+    frameFile: { type: String, default: "" },
+    transitionPlacement: { type: String, default: "" },
+    transitionType: { type: String, default: "" },
+    profilePosition: { type: String, default: "" },
+    profileSize: { type: String, default: "" },
+    profileShape: { type: String, default: "" },
+    isImage: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Share", shareSchema);
 
